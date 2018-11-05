d099aac1fdfb6f83321a5cf679c7e779eb126046
Jerry Fernandez <gmfernandez@cpp.edu>
2018-11-04 17:37:53 -0800

    Basic set creation completed.

--- main.cpp
+++ main.cpp
@@ -11,9 +11,9 @@ bool isNum(const string& st);
 int convertString(const string& st);
 int wordCount(string set);
 void insertionSort(int n, int _array[]);
-void removeDuplicates(int n, int _array[]);
-//remove duplicates from set
-//sort int array
+void markDuplicates(int n, int _array[]);
+int countUnique(int n, int _array[]);
+
 
 
 int main() {
@@ -21,10 +21,8 @@ int main() {
 
     int *p;//pointer to array of ints
     int word_count = 0; //used to dynamically generate standard string set arrays
-
     string set_a = "";
-
-//    Initialize first set
+    //Initialize first set
     std::cout << "Please enter a set of  positive integers :\n" << std::endl;
     set_a = getSet();
     word_count = wordCount(set_a);
@@ -33,49 +31,50 @@ int main() {
     int set_one[word_count];
     p = set_one;
     stripSpace(set_a, p);
+    insertionSort(word_count, set_one);
+    markDuplicates(word_count, set_one);
+    int fin_size_a = countUnique(word_count, set_one);
+    int final_s1[fin_size_a];
+    int inner = 0;
+    for (int i = 0;i<word_count;i++)
+        if (set_one[i] != -1)
+         final_s1[inner++] = set_one[i];
 
     //    Initialize second set
     int *q;
     string set_b = "";
-
     std::cout << "Please enter a second set of positive integers :\n" << std::endl;
     set_b = getSet();
     int word_count_two = wordCount(set_b);
     int set_two[word_count_two];
     q = set_two;
     stripSpace(set_b, q);
+    insertionSort(word_count_two, set_two);
+    markDuplicates(word_count_two, set_two);
+    int fin_size_b = countUnique(word_count_two, set_two);
+    int final_s2[fin_size_b];
+    int inner_b = 0;
+    for (int i = 0;i<word_count_two;i++)
+        if (set_two[i] != -1)
+            final_s2[inner_b++] = set_two[i];
 
-    for (int i = 0; i < word_count; i++)
-        std::cout<< set_one[i] <<' ';
+//     std::cout<< "Post pruning the sets are \n";
+//
+//    for (int i = 0; i < fin_size_a; i++)
+//        std::cout<< final_s1[i] <<' ';
+//
+//    std::cout<< '\n';
+//
+//    for (int i = 0; i < fin_size_b; i++)
+//        std::cout<< final_s2[i] <<' ';
+//
+//    std::cout<< '\n';
 
-    std::cout<< '\n';
 
-    for (int i = 0; i < word_count_two; i++)
-        std::cout<< set_two[i] <<' ';
-
-    std::cout<< '\n';
-
-    insertionSort(word_count, set_one);
-
-    std::cout<< "Post Insertion Sort " <<std::endl;
-
-    for (int i = 0; i < word_count; i++)
-        std::cout<< set_one[i] <<' ';
-
-    std::cout<< '\n';
-
-    removeDuplicates(word_count, set_one);
-
-    std::cout<< "Post Removal " <<std::endl;
-
-    for (int i = 0; i < word_count; i++)
-        if (set_one[i] != -1)
-         std::cout<< set_one[i] <<' ';
     p = NULL;
     q = NULL;
     return 0;
 }
-
 
 string getSet()
 {
@@ -114,7 +113,7 @@ void stripSpace(string set, int *p)
         //check for numeric properties
         //if yes push on int stack
         string temp = set.substr(0, index);
-
+//        temp = delSpaces(temp);
         if (isNum(temp))
             numbers.push(convertString(temp));
             //first_words.push(temp);
@@ -138,6 +137,9 @@ void stripSpace(string set, int *p)
 }
 
 bool isNum(const string& st){
+    if (st == "")
+        return false;
+
     for (int i=0;i<st.size();i++)
         if (!isdigit(st[i]))
             return false;
@@ -146,8 +148,8 @@ bool isNum(const string& st){
 
 int convertString(const string& st){
     try {
-        std::string::size_type reference;
-        return  std::stof (st, &reference);
+        //string temp = st;
+        return  std::stoi (st);
     }
     catch (int e)
     {
@@ -172,11 +174,20 @@ void insertionSort(int n, int _array[]){
     }
 }
 
-void removeDuplicates(int n, int _array[]) {
+void markDuplicates(int n, int _array[]) {
     int i, j;
     for (i=0; i<n; i++){
         for(j=i+1; j<n; j++)
             if(_array[i] == _array[j])
                 _array[j] = -1;
     }
+}
+
+int countUnique(int n, int _array[]){
+    int counter =0;
+        for (int i = 0; i < n; i++){
+            if (_array[i] != -1)
+                counter++;
+        }
+    return counter;
 }
