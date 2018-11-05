c3964880a2cece3f5bdc250f07fe30c3d8a0ae57
Jerry Fernandez <gmfernandez@cpp.edu>
2018-11-04 23:17:03 -0800

    Part 1 of program complete.

--- main.cpp
+++ main.cpp
@@ -13,8 +13,14 @@ int wordCount(string set);
 void insertionSort(int n, int _array[]);
 void markDuplicates(int n, int _array[]);
 int countUnique(int n, int _array[]);
-
-
+void menu(const int set_one[], const int set_two[], const int size_a, const int size_b);
+void _union(const int set_one[], const int set_two[], const int size_a, const int size_b);
+void intersection(const int set_one[], const int set_two[], const int size_a, const int size_b);
+void difference(const int set_one[], const int set_two[], const int size_a, const int size_b);
+void my_exit();
+void space();
+void displaySets(const int set_one[], const int set_two[], const int size_a, const int size_b);
+void  markAllDuplicates(int n, int _array[]);
 
 int main() {
 
@@ -60,21 +66,13 @@ int main() {
         if (set_two[i] != -1)
             final_s2[inner_b++] = set_two[i];
 
-//     std::cout<< "Post pruning the sets are \n";
-//
-//    for (int i = 0; i < fin_size_a; i++)
-//        std::cout<< final_s1[i] <<' ';
-//
-//    std::cout<< '\n';
-//
-//    for (int i = 0; i < fin_size_b; i++)
-//        std::cout<< final_s2[i] <<' ';
-//
-//    std::cout<< '\n';
+
+    displaySets(final_s1, final_s2, fin_size_a, fin_size_b);
 
 
     p = NULL;
     q = NULL;
+
     return 0;
 }
 
@@ -192,4 +190,183 @@ int countUnique(int n, int _array[]){
                 counter++;
         }
     return counter;
+}
+
+void menu(const int set_one[], const int set_two[], const int size_a, const int size_b){
+    char choice;
+    std::cout<< "Please choose from one of the following set operations :\n";
+    std::cout<< "Union: Press U/u\n";
+    std::cout<< "Intersection: Press I/i\n";
+    std::cout<< "Difference: Press D/d\n";
+    std::cout<< "Display sets: A/a\n";
+    std::cout<< "Exit: E/e\n";
+    //get user input
+    std::cin >> choice;
+    choice = toupper(choice);
+    switch(choice)
+    {
+        case 'U':  _union(set_one, set_two, size_a, size_b);
+            break;
+        case 'I': intersection(set_one, set_two, size_a, size_b);
+            break;
+        case 'D': difference(set_one, set_two, size_a, size_b);
+            break;
+        case 'A': displaySets(set_one, set_two, size_a, size_b);
+            break;
+        case 'E': my_exit();
+            break;
+        default:
+            std::cout<< "Incorrect choice, please try again.\n";
+            menu(set_one, set_two, size_a, size_b);
+    }
+
+}
+
+void _union(const int set_one[], const int set_two[], const int size_a, const int size_b){
+
+    int temp = size_a + size_b;
+    int array[temp];
+    for (int i=0; i < size_a; i++)
+        array[i] = set_one[i];
+    int y=size_a;
+    for (int j=0; j <size_b; j++)
+        array[y++] = set_two[j];
+
+
+    insertionSort(temp, array);
+    markDuplicates(temp, array);
+
+    std::cout<<"The union of both sets is \n";
+
+    for (int i=0; i < temp; i++)
+        if(array[i] != -1)
+        std::cout<< array[i] << ' ';
+
+    std::cout<<"\n";
+
+    menu(set_one, set_two, size_a, size_b);
+}
+
+void intersection(const int set_one[], const int set_two[], const int size_a, const int size_b){
+
+    int temp = size_a + size_b;
+    int array[temp];
+    for (int i=0; i < size_a; i++)
+        array[i] = set_one[i];
+    int y=size_a;
+    for (int j=0; j <size_b; j++)
+        array[y++] = set_two[j];
+
+
+    insertionSort(temp, array);
+    stack<int> int_stk;
+    stack<int> reverse_stk;
+    int i, j;
+    for (i=0; i<temp; i++){
+        for(j=i+1; j<temp; j++)
+            if(array[i] == array[j])
+               int_stk.push(array[i]);
+    }
+
+    //reverse stack
+    while(!int_stk.empty()){
+        reverse_stk.push(int_stk.top());
+        int_stk.pop();
+    }
+    std::cout<< "The intersection of both sets is\n";
+
+    if (reverse_stk.empty()){
+        space();
+        std::cout<< "The Empty set\n{}\n";
+    }
+
+    while(!reverse_stk.empty()){
+        std::cout<< reverse_stk.top() << ' ';
+        reverse_stk.pop();
+    }
+    std::cout<< "\n";
+    space();
+    menu(set_one, set_two, size_a, size_b);
+}
+void difference(const int set_one[], const int set_two[], const int size_a, const int size_b){
+    int temp = size_a + size_b;
+    int array[temp];
+    for (int i=0; i < size_a; i++)
+        array[i] = set_one[i];
+    int y=size_a;
+    for (int j=0; j <size_b; j++)
+        array[y++] = set_two[j];
+
+
+    insertionSort(temp, array);
+    markAllDuplicates(temp, array);
+    stack<int> int_stk;
+    stack<int> reverse_stk;
+
+    int i;
+    for (i=0; i<temp; i++){
+        int_stk.push(array[i]);
+        if(array[i] != -1){
+            int_stk.push(array[i]);
+        }
+
+    }
+    //reverse stack
+    while(!int_stk.empty()){
+        if (int_stk.top() != -1)
+            reverse_stk.push(int_stk.top());
+        int_stk.pop();
+    }
+    std::cout<< "The difference in the sets is\n";
+
+    if (reverse_stk.empty()){
+        space();
+        std::cout<< "The Empty set\n{}\n";
+    }
+
+    while(!reverse_stk.empty()){
+        std::cout<< reverse_stk.top() << ' ';
+        reverse_stk.pop();
+        reverse_stk.pop();
+    }
+    std::cout<< "\n";
+    space();
+    menu(set_one, set_two, size_a, size_b);
+}
+
+void my_exit(){
+    std::cout<< "Goodbye!\n";
+    exit(0);
+}
+
+void space(){
+    std::cout<< "----------------------------\n";
+}
+
+void displaySets (const int set_one[], const int set_two[], const int size_a, const int size_b){
+    std::cout<< "The sets are \n";
+
+    for (int i = 0; i < size_a; i++)
+        std::cout<< set_one[i] <<' ';
+
+    std::cout<< '\n';
+
+    for (int i = 0; i < size_b; i++)
+        std::cout<< set_two[i] <<' ';
+
+    std::cout<< '\n';
+    space();
+    menu(set_one, set_two, size_a, size_b);
+}
+
+void markAllDuplicates(int n, int _array[]){
+    int i, j;
+    for (i=0; i<n; i++){
+        for(j=i+1; j<n; j++)
+            if(_array[i] == _array[j]){
+                _array[j] = -1;
+                _array[i] = -1;
+            }
+
+    }
 }
